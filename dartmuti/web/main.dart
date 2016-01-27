1990dd88619fd1eae1f71e58b96d6f2f13d198db
isaacbernat <bernat@wrapp.com>
2016-01-27 23:57:52 +0100

    Initial commit of the angular2 app

---
+++ dartmuti/web/main.dart
@@ -0,0 +1,8 @@
+import 'package:angular2/angular2.dart';
+import 'package:angular2/bootstrap.dart';
+@Component(selector: 'jsnabi-main', template: '<h1>Dummy page</h1>')
+class AppComponent {}
+main() {
+  bootstrap(AppComponent);
+}
+
