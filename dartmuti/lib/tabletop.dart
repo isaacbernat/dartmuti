4de9e04f13921f7a411ab05196e72458514fcb9d
isaacbernat <bernat@wrapp.com>
2016-02-14 16:28:34 +0100

    Shuffle cards and better deck initialisation

--- dartmuti/lib/tabletop.dart
+++ dartmuti/lib/tabletop.dart
@@ -1,13 +1,15 @@
+import 'dart:math';
 import 'card.dart';
 import 'trick.dart';
 
 class Tabletop {
   String name;
+  int seed;
   List<Card> deck;
   List<Card> discardPile = [];
   List<Trick> currentTricks = [];
   List<int> currentPassedPlayers = [];
-  List<String> roles = [
+  List<String> roleNames = [
     "The Great Dalmuti",
     "Archbishop",
     "Earl Marshal",
@@ -21,29 +23,44 @@ class Tabletop {
     "Stonecutter",
     "Peasants"
   ];
+  Observable<List<Map>> roles = [];  // each element contains name, value, amount
 
   Tabletop(this.name,
       [this.discardPile, this.currentTricks, this.currentPassedPlayers]);
 
   void init() {
-    discardPile = buildDeck(3, roles);
-    deck = buildDeck(12, roles);
+    seed = 1337;
+    roles = initRoles(roleNames);
+    deck = buildDeck(roles);
     currentTricks = [
       new Trick([deck[10]]),
       new Trick([deck[6]]),
       new Trick([deck[3], deck[4]]),
       new Trick([deck[1], deck[2]]),
     ];
-    deck = deck.reversed.toList();
+    deck.shuffle(new Random(seed));
   }
 
-  List<Card> buildDeck(int highestValue, [List<String> roles]) {
+  List<Map> initRoles(List<String> roleNames) {
+    Observable<List<Map>> roles = [];
+    for (int value = 1; value < roleNames.length + 1; value++) {
+      Map role = {
+        "name": roleNames[value -1],
+        "value": value,
+        "amount": value,  // by default there are N cards with value N
+      };
+      roles.add(role);
+    }
+    return roles;
+  }
+
+  List<Card> buildDeck(List<Map> roles) {
     List<Card> cards = [];
     int cardID = 0;
-    for (int value = 1; value < highestValue + 1; value++) {
-      for (int i = 0; i < value; i++) {
+    for (int i = 0; i < roles.length; i++) {
+      for (int j = 0; j < roles[i]['amount']; j++) {
         ++cardID;
-        cards.add(new Card(cardID, value, roles[value - 1]));
+        cards.add(new Card(cardID, roles[i]['value'], roles[i]['name']));
       }
     }
     return cards;
